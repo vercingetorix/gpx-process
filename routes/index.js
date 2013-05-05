9ed089f7749845dd3266b8bec30618b20f5bd7c5
Sudhanshu Raheja <sudhanshu@vxtindia.com>
2013-05-05 21:08:11 +0530

    tracking fastest times from the start

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,13 @@ function printRecords() {
 	for(x in times) {
 		console.log(
 			times[x].name,
+			' : ',
 			times[x].time.toPrecision(5),
+			' : ',
 			times[x].speed.toPrecision(6),
+			' : ',
 			times[x].pace.toPrecision(5),
+			' : ',
 			times[x].dated
 		);
 	}
@@ -63,21 +67,66 @@ function findFastest(points, distance, name) {
 
 	if( !_.isUndefined(fastestTime[0]) ) {
 		if( _.isUndefined(times[name]) ) {
-			console.log(name + ' : NEW : ' + fastestTime[0].speed + ' : DATE : ' + fastestTime[0].dated);
+			console.log(
+				name,
+				' : ',
+				fastestTime[0].speed.toFixed(4),
+				' : ',
+				fastestTime[0].pace.toFixed(4),
+				' : ',
+				fastestTime[0].dated,
+				' : ',
+				fastestTime[0].time
+			);
 			times[name] = fastestTime[0];
 		} else {
 			if( fastestTime[0].pace < times[name].pace ) {
-				console.log(name + ' : UPD : ' + fastestTime[0].speed + ' : DATE : ' + fastestTime[0].dated + ' : OLD : ' + times[name].speed);
+				console.log(
+					name,
+					' : ',
+					fastestTime[0].speed.toFixed(4),
+					' : ',
+					fastestTime[0].pace.toFixed(4),
+					' : ',
+					fastestTime[0].time,
+					' : ',
+					fastestTime[0].dated,
+					' : ',
+				 	(fastestTime[0].speed - times[name].speed).toFixed(4),
+					' : ',
+				 	(times[name].pace - fastestTime[0].pace).toFixed(4),
+				 	' : ',
+				 	(times[name].time - fastestTime[0].time)
+				);
 				times[name] = fastestTime[0];
+			} else {
+				console.log(
+					' : ',
+					name,
+					' : ',
+					fastestTime[0].speed.toFixed(4),
+					' : ',
+					fastestTime[0].pace.toFixed(4),
+					' : ',
+					fastestTime[0].time,
+					' : ',
+					fastestTime[0].dated,
+					' : ',
+				 	(fastestTime[0].speed - times[name].speed).toFixed(4),
+					' : ',
+				 	(times[name].pace - fastestTime[0].pace).toFixed(4),
+				 	' : ',
+				 	(times[name].time - fastestTime[0].time)
+				);
 			}
 		}
 	}
 
 	//console.log(times);
-	printRecords();
+	//printRecords();
 };
 
-function processData(filePath, url) {
+function processData(filePath, url, callback) {
 	fs.readFile(filePath + '.gpx', 'utf8', function(err, data) {
 		if(err) console.log('URL: ' + url + '/export_gpx, FILE: ' + filePath + ', ERROR: ' + err);
 		else {
@@ -113,8 +162,16 @@ function processData(filePath, url) {
 						totalDistance += point.distanceDelta;
 						point.totalDistance = totalDistance;
 
-						lastPoint = point;
-						points.push(point);
+						point.speed = _.isEmpty(points) ? 0 : (point.distanceDelta / (point.time - lastPoint.time)) * (3600 / 1000);
+						//console.log(point.speed);
+
+						//if(point.speed > 36) {
+							lastPoint = point;
+							points.push(point);
+						//} else {
+						//	console.log(points);
+						//}
+
 						i++;
 					});
 
@@ -135,9 +192,12 @@ function processData(filePath, url) {
 						{ name: '20km___', distance: 20000 }
 					];
 
+					//console.log(points);
 					_.each(distances, function(distance) {
 						findFastest(points, distance.distance, distance.name);
 					});
+
+					callback();
 				}
 			});
 		}
@@ -157,9 +217,15 @@ exports.index = function() {
 	cycling.push('http://app.strava.com/activities/3728493');
 	cycling.push('http://app.strava.com/activities/3755042');
 
-	cycling.forEach(function(data) {
-		processData('downloads/' + utils.md5(data), data);
-	});	
+	// var cycling_count = cycling.length;
+	// cycling.forEach(function(data) {
+	// 	processData('downloads/' + utils.md5(data), data, function() {
+	// 		cycling_count--;
+	// 		if(cycling_count == 0) {
+	// 			printRecords();
+	// 		}
+	// 	});
+	// });	
 
 	var running = [];
 	running.push('http://app.strava.com/activities/3755140');
@@ -170,7 +236,7 @@ exports.index = function() {
 	running.push('http://app.strava.com/activities/3901642');
 	running.push('http://app.strava.com/activities/3926649');
 	running.push('http://app.strava.com/activities/3955433');
-	running.push('http://app.strava.com/activities/3978508');
+	//running.push('http://app.strava.com/activities/3978508');
 	running.push('http://app.strava.com/activities/4003948');
 	running.push('http://app.strava.com/activities/4055062');
 	running.push('http://app.strava.com/activities/4085686');
@@ -189,26 +255,26 @@ exports.index = function() {
 	running.push('http://app.strava.com/activities/4720217');
 	running.push('http://app.strava.com/activities/4765086');
 	running.push('http://app.strava.com/activities/4813419');
-	running.push('http://app.strava.com/activities/4849737');
+	//running.push('http://app.strava.com/activities/4849737');
 	running.push('http://app.strava.com/activities/4956594');
 	running.push('http://app.strava.com/activities/5007546');
 	running.push('http://app.strava.com/activities/5058907');
 	running.push('http://app.strava.com/activities/5186605');
-	running.push('http://app.strava.com/activities/5229746');
+	//running.push('http://app.strava.com/activities/5229746');
 	running.push('http://app.strava.com/activities/5467097');
 	running.push('http://app.strava.com/activities/5605077');
 	running.push('http://app.strava.com/activities/5697032');
 	running.push('http://app.strava.com/activities/5826715');
 	running.push('http://app.strava.com/activities/5980992');
 	running.push('http://app.strava.com/activities/6037524');
-	running.push('http://app.strava.com/activities/6073828');
+	//running.push('http://app.strava.com/activities/6073828');
 	running.push('http://app.strava.com/activities/8451248');
 	running.push('http://app.strava.com/activities/8944224');
 	running.push('http://app.strava.com/activities/13471109');
 	running.push('http://app.strava.com/activities/19140106');
 	running.push('http://app.strava.com/activities/19142268');
 	running.push('http://app.strava.com/activities/19262814');
-	running.push('http://app.strava.com/activities/19783929');
+	//running.push('http://app.strava.com/activities/19783929');
 	running.push('http://app.strava.com/activities/20581514');
 	running.push('http://app.strava.com/activities/21384122');
 	running.push('http://app.strava.com/activities/22020584');
@@ -245,5 +311,23 @@ exports.index = function() {
 	running.push('http://app.strava.com/activities/51718365');
 	running.push('http://app.strava.com/activities/52267482');
 
-	//processData('public/run.gpx');
+	// Buggy Codes
+	//running = [];
+	//running.push('http://app.strava.com/activities/3978508');
+	//running.push('http://app.strava.com/activities/4849737');
+	//running.push('http://app.strava.com/activities/5229746');
+	//running.push('http://app.strava.com/activities/6073828');
+	//running.push('http://app.strava.com/activities/19783929');
+
+	var running_count = running.length;
+	running.forEach(function(data) {
+		processData('downloads/' + utils.md5(data), data, function() {
+			console.log('Processed (' + utils.md5(data)  + ') ' + data);
+			running_count--;
+			if(running_count == 0) {
+				printRecords();
+			}
+		});
+	});	
+
 };
