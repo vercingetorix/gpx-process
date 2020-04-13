6edb60e11185884970d69a6ad4f28ae1d86fe173
Sudhanshu Raheja <sudhanshu@leftshift.io>
2020-04-13 14:50:33 +0700

    [sudhanshu] migrated code to the latest version of packages

--- routes/geometry.js
+++ routes/geometry.js
@@ -1,18 +1,22 @@
-var geometry = {
+const geometry = {
+  earthRadius: 6371000,
 
-	earthRadius: 6371000,
+  toRadians: (degrees) => {
+    return (degrees * Math.PI) / 180;
+  },
 
-	toRadians: function(degrees) {
-		return degrees * Math.PI / 180;
-	},
-
-	getDistance: function(lat1, long1, lat2, long2) {
-		var a, c, dLat = geometry.toRadians(lat2 - lat1), dLong = geometry.toRadians(long2 - long1);
-		a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLong / 2) * Math.sin(dLong / 2) * Math.cos(geometry.toRadians(lat1)) * Math.cos(geometry.toRadians(lat2));
-		c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		return c * geometry.earthRadius;
-	}
-
+  getDistance: (lat1, long1, lat2, long2) => {
+    const dLat = geometry.toRadians(lat2 - lat1);
+    const dLong = geometry.toRadians(long2 - long1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLong / 2) *
+        Math.sin(dLong / 2) *
+        Math.cos(geometry.toRadians(lat1)) *
+        Math.cos(geometry.toRadians(lat2));
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return c * geometry.earthRadius;
+  },
 };
 
 exports.geometry = geometry;
--- routes/index.js
+++ routes/index.js
@@ -1,114 +1,116 @@
-var geometry = require('./geometry').geometry;
-var parseString = require('xml2js').parseString;
-var fs = require('fs');
-var moment = require('moment');
-var _ = require('underscore');
+/* eslint-disable no-console */
+const fs = require("fs");
+const moment = require("moment");
+const _ = require("underscore");
+const { parseString } = require("xml2js");
+const { geometry } = require("./geometry");
 
 function findFastest(points, distance) {
-	var x = 0;
-	var fastestTimes = [];
-	points.forEach(function(data) {
-		if(data.totalDistance > distance) {
-			//console.log('comparing ' + x + ', actual distance = ' + (data.totalDistance - distance));
-			var x2 = 0;
-			var foundMatch = false;
-			points.forEach(function(data2) {
-				var diff = data.totalDistance - data2.totalDistance;
-				if(diff > 0) {
-					if( !foundMatch && (diff < distance) ) {
-						//console.log('comparing ' + x + ' with ' + x2);
-						//console.log('diff ' + diff + ', distance ' + distance);
-						//console.log('matching on ' + (diff - distance));
-						foundMatch = true;
-						var selectedPoint = points[x2 - 1];
+  let x = 0;
+  const fastestTimes = [];
+  points.forEach((data) => {
+    if (data.totalDistance > distance) {
+      // console.log('comparing ' + x + ', actual distance = ' + (data.totalDistance - distance));
+      let x2 = 0;
+      let foundMatch = false;
+      points.forEach((data2) => {
+        const diff = data.totalDistance - data2.totalDistance;
+        if (diff > 0) {
+          if (!foundMatch && diff < distance) {
+            // console.log('comparing ' + x + ' with ' + x2);
+            // console.log('diff ' + diff + ', distance ' + distance);
+            // console.log('matching on ' + (diff - distance));
+            foundMatch = true;
+            const selectedPoint = points[x2 - 1];
 
-						fastestTime = {};
-						fastestTime.time = data.runTime - selectedPoint.runTime;
-						fastestTime.distance = data.totalDistance - selectedPoint.totalDistance;
-						fastestTime.start = points[x2 - 1];
-						fastestTime.end = points[x];
-						fastestTime.pace = (fastestTime.time / fastestTime.distance) * (1000 / 60);
-						fastestTime.adjustedTime = (fastestTime.time * distance) / fastestTime.distance;
+            const fastestTime = {};
+            fastestTime.time = data.runTime - selectedPoint.runTime;
+            fastestTime.distance =
+              data.totalDistance - selectedPoint.totalDistance;
+            fastestTime.start = points[x2 - 1];
+            fastestTime.end = points[x];
+            fastestTime.pace =
+              (fastestTime.time / fastestTime.distance) * (1000 / 60);
+            fastestTime.adjustedTime =
+              (fastestTime.time * distance) / fastestTime.distance;
 
-						//console.log('diff ' + fastestTime.distance + ', time ' + fastestTime.time + ', pace ' + fastestTime.pace);
+            // console.log('diff ' + fastestTime.distance + ', time ' + fastestTime.time + ', pace ' + fastestTime.pace);
 
-						fastestTimes.push( fastestTime );
+            fastestTimes.push(fastestTime);
+            // console.log(fastestTimes);
+          }
+        }
+        x2 += 1;
+      });
+    } else {
+      // console.log('totalDistance too short (' + data.totalDistance + ') : ignoring');
+    }
+    x += 1;
+  });
 
-						//console.log(fastestTimes);
-					}
-				}
-				x2++;
-			});
-		} else {
-			//console.log('totalDistance too short (' + data.totalDistance + ') : ignoring');
-		}
-		x++;
-	});
+  const times = _.sortBy(fastestTimes, (trackPoint) => {
+    return trackPoint.pace;
+  });
 
-	var times = _.sortBy(fastestTimes, function(trackPoint) {
-		return trackPoint.pace;
-	});
-
-	//return _.isEmpty(times) ? {} : times[0];
-	return times;
-};
-
-function processData(result) {
-	var trackPoints = result.gpx.trk[0].trkseg[0].trkpt;
-
-	var i = 0;
-	var points = [];
-	var firstPoint = {};
-	var lastPoint = {};
-	var totalDistance = 0;
-
-	trackPoints.forEach(function(data) {
-		var point = {};
-		point.i = i;
-		point.latitude = data.$.lat;
-		point.longitude = data.$.lon;
-		point.elevation = data.ele[0];
-		point.time = moment( data.time[0] ).format('X');
-
-		firstPoint = _.isEmpty(points) ? point : points[0];
-		
-		point.runTime = point.time - firstPoint.time;
-		point.distanceDelta = _.isEmpty(points) ? 0 : geometry.getDistance(
-				lastPoint.latitude,
-				lastPoint.longitude,
-				point.latitude,
-				point.longitude
-			);
-
-		totalDistance += point.distanceDelta;
-		point.totalDistance = totalDistance;
-
-		lastPoint = point;
-		points.push(point);
-		i++;
-	});
-	//console.log(points);
-
-	//var fastest400 = findFastest(points, 400);
-	_.each([100, 200, 400, 800, 1500, 5000, 10000], function(distance) {
-		var fastest = findFastest(points, distance);
-		console.log( fastest );
-	})
-	//console.log(fastest400);
-
+  // return _.isEmpty(times) ? {} : times[0];
+  return times;
 }
 
+function processData(result) {
+  const trackPoints = result.gpx.trk[0].trkseg[0].trkpt;
 
+  let i = 0;
+  const points = [];
+  let firstPoint = {};
+  let lastPoint = {};
+  let totalDistance = 0;
 
-//exports.index = function(req, res){
-exports.index = function() {
-	fs.readFile('public/run.gpx', 'utf8', function(err, data) {
-		if(err) console.log(err);
-		else {
-			parseString(data, function (err, result) {
-				if(err) console.log(err);
-				else processData(result);
-			});
-		}
-	});
+  trackPoints.forEach((data) => {
+    const point = {};
+    point.i = i;
+    point.latitude = data.$.lat;
+    point.longitude = data.$.lon;
+    [point.elevation] = data.ele;
+    point.time = moment(data.time[0]).format("X");
+
+    firstPoint = _.isEmpty(points) ? point : points[0];
+
+    point.runTime = point.time - firstPoint.time;
+    point.distanceDelta = _.isEmpty(points)
+      ? 0
+      : geometry.getDistance(
+          lastPoint.latitude,
+          lastPoint.longitude,
+          point.latitude,
+          point.longitude
+        );
+
+    totalDistance += point.distanceDelta;
+    point.totalDistance = totalDistance;
+
+    lastPoint = point;
+    points.push(point);
+    i += 1;
+  });
+  // console.log(points);
+
+  // var fastest400 = findFastest(points, 400);
+  _.each([100, 200, 400, 800, 1500, 5000, 10000], (distance) => {
+    const fastest = findFastest(points, distance);
+    console.log(fastest);
+  });
+  // console.log(fastest400);
+}
+
+// exports.index = function(req, res){
+exports.index = () => {
+  fs.readFile("public/run.gpx", "utf8", (err, data) => {
+    if (err) console.log(err);
+    else {
+      parseString(data, (err, result) => {
+        if (err) console.log(err);
+        else processData(result);
+      });
+    }
+  });
 };
